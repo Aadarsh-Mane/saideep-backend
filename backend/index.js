62e9b1206dad841f099d44288b6988235a1dc0f9
aadarsh <onlyaddy68@gmail.com>
2024-12-06 16:09:55 +0530

    checking location route

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,26 @@ app.get("/", (req, res) => {
 
   console.log("welcome to doctors service");
 });
+
+const predefinedLocation = {
+  latitude: 19.215696,
+  longitude: 73.0804656,
+};
+
+app.post("/check-location", (req, res) => {
+  const { latitude, longitude } = req.body;
+
+  // Calculate if the given coordinates are close enough to the predefined location
+  const isInLocation =
+    Math.abs(predefinedLocation.latitude - latitude) < 0.0001 &&
+    Math.abs(predefinedLocation.longitude - longitude) < 0.0001;
+
+  if (isInLocation) {
+    res.json({ message: "You are in the right location!" });
+  } else {
+    res.json({ message: "You are not in the correct location." });
+  }
+});
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
