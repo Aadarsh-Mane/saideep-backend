e7a6ffcf2a0c095fe146c65b22a9e79e979c1ff2
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-02-03 02:03:34 +0530

    added twilio

--- backend/helpers/twilio.js
+++ backend/helpers/twilio.js
@@ -1,5 +1,5 @@
 import twilio from "twilio";
 const TWILIO_ACCOUNT_SID = "AC35d86e0d9c60d2eb91c76053c7c863e1";
-const TWILIO_AUTH_TOKEN = "ee3d620954c9e24f4388300475d433e7";
+const TWILIO_AUTH_TOKEN = "7f9dc6bdf9cc474dcef00e303cf190fc";
 
 export const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
