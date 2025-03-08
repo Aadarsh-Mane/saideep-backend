41215e21bd2e2a5f96ad597a6ffc1002ff7eb47d
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-03-09 01:48:36 +0530

    now

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/labs", labRouter);
 app.get("/patientHistory/:patientId", getPatientHistory);
 
 app.get("/", (req, res) => {
-  return res.status(200).json("Welcome to Ai in HealthCare v1");
+  return res.status(200).json("Welcome to Ai in HealthCare v2");
 });
 app.post("/generateDischargeSummary", async (req, res) => {
   const patientData = req.body; // Patient details sent from Flutter
